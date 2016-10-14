585d4b2ae12a91e3cd4dd38337671e65b49f417f
urigo <uri.goldshtein@gmail.com>
2016-10-14 17:01:25 +0200

    chore(move): Move repo to the main angular-meteor repo

--- atmosphere-packages/angular2-with-blaze-compilers/plugin/register.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Plugin.registerCompiler({
-  extensions: ['ng2.html']
-}, () => new HtmlCompiler());
-
-Plugin.registerCompiler({
-  extensions: ['less']
-}, () => new StyleCompiler());
